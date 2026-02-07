bd6ab091defb1df0c4fa7f30c4d49fbd90098c7b
Pavan Naik <itspavannaik001@gmail.com>
2026-02-08 01:25:32 +0530

    update styles and minor fixes

--- js/app.js
+++ js/app.js
@@ -485,6 +485,6 @@ init();
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("sw.js");
+    navigator.serviceWorker.register("./sw.js");
   });
 }
