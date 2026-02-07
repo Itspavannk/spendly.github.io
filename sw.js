d0f59f9f9f74b6c4dfba2943a49061448a30c8db
Pavan Naik <itspavannaik001@gmail.com>
2026-02-08 01:32:56 +0530

    update styles and minor fixes

--- sw.js
+++ sw.js
@@ -1,20 +1,44 @@
 const CACHE_NAME = "spendly-v1";
+
 const ASSETS = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/js/app.js",
-  "/manifest.json"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./css/style.css",
+  "./js/app.js",
+  "./assets/icons/icon-192.png",
+  "./assets/icons/icon-512.png"
 ];
 
+// Install
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
   );
 });
